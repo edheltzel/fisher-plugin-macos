7cdf704d43608761f7dd35eb79e493e1740090c7
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2023-10-12 15:10:37 -0400

    rmds function includes switch case

--- functions/rmds.fish
+++ functions/rmds.fish
@@ -1,9 +1,4 @@
-# function rmds -d "Remove .DS_Store files recursively in a directory, default"
-#     set cwd .
-#     find $cwd -maxdepth 1 -type f -name .DS_Store -delete
-# end
-# Remove .DS_Store files either recursively in a directory or only in the current directory.
-function rmds
+function rmds -d "Remove .DS_Store files either recursively in a directory or only in the current directory"
     switch $argv[1]
         case -r or --recursive
             set cwd $argv[2]
@@ -12,6 +7,6 @@ function rmds
             set cwd .
             find $cwd -maxdepth 1 -type f -name .DS_Store -delete
         case '*'
-            echo "Usage: rmds [-r|--recursive DIRECTORY] [-c|--current]"
+            echo "Usage: rmds [-r|--recursive /path/to/directory] [-c|--current]"
     end
 end
