155f047e91714fb92d6a3c6bf355746b0cf6e31e
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2023-10-12 14:24:52 -0400

    fixing find command error with `rmds`

--- functions/rmds.fish
+++ functions/rmds.fish
@@ -1,3 +1,3 @@
 function rmds -d "Remove .DS_Store files recursively in a directory, default"
-    find $argv[1..-1] -type f -name .DS_Store -delete
+    find $argv[1..-1] -name .DS_Store -print | xargs rm -f
 end
