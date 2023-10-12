9dbb4120e48fd1b92cc8e488746e93a1fc9c4861
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2023-10-12 14:51:56 -0400

    rmds function update to include flags/options

--- functions/rmds.fish
+++ functions/rmds.fish
@@ -1,3 +1,17 @@
-function rmds -d "Remove .DS_Store files recursively in a directory, default"
-    find $argv[1..-1] -type f -name .DS_Store -exec rm -f {} \;
+# function rmds -d "Remove .DS_Store files recursively in a directory, default"
+#     set cwd .
+#     find $cwd -maxdepth 1 -type f -name .DS_Store -delete
+# end
+# Remove .DS_Store files either recursively in a directory or only in the current directory.
+function rmds
+    switch $argv[1]
+        case -r or --recursive
+            set cwd $argv[2]
+            find $cwd -type f -name .DS_Store -delete
+        case -c or --current
+            set cwd .
+            find $cwd -maxdepth 1 -type f -name .DS_Store -delete
+        case '*'
+            echo "Usage: rmds [-r|--recursive DIRECTORY] [-c|--current]"
+    end
 end
